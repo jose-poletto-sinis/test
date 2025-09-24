a911bbfa8aadfe6615555b4f483dd93ebc097aa7
jose-poletto-sinis <jose.poletto@sinis.com.ar>
2025-09-24 08:18:50 -0300

    feat: Refactor EmployeesGrid to use gridChildren for improved structure and add export functionality

--- my-react-router-app/app/routes/local-data.tsx
+++ my-react-router-app/app/routes/local-data.tsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
 import { Loader } from '@progress/kendo-react-indicators';
-
 import { GridPDFExport } from '@progress/kendo-react-pdf';
 import { ExcelExport } from '@progress/kendo-react-excel-export';
 import { Button, ButtonGroup } from '@progress/kendo-react-buttons';
 import { Grid as KendoGrid, GridToolbar, GridSearchBox } from '@progress/kendo-react-grid';
-
 import { BudgetCell, ColumnMenu, PersonCell, ProgressCell, RatingCell, CountryCell } from '../components/custom-cells';
-import { employees } from '../routes/employees';
+import { employees } from './employees';
 
 const styles = {
     height: '100%',
@@ -69,6 +67,78 @@ const EmployeesGrid = () => {
         _pdfExport.save();
     };
 
+    // Crea dos grids independientes para evitar el error
+    const gridChildren = (
+        <>
+            <GridToolbar>
+                <GridSearchBox style={{ width: 210 }} />
+                <ButtonGroup>
+                    <Button themeColor={'base'} onClick={exportExcel}>
+                        Export to Excel
+                    </Button>
+                    <Button themeColor={'base'} onClick={exportPDF}>
+                        Export to PDF
+                    </Button>
+                </ButtonGroup>
+            </GridToolbar>
+            <Column filterable={false} groupable={false} width={50} columnType="checkbox" />
+            <Column title="Employee" groupable={false} sortable={false}>
+                <Column
+                    field="full_name"
+                    title="Contact Name"
+                    columnMenu={ColumnMenu}
+                    cells={{
+                        data: PersonCell
+                    }}
+                    width="250px"
+                />
+                <Column field="job_title" title="Job Title" columnMenu={ColumnMenu} width="220px" />
+                <Column
+                    field="country"
+                    title="Country"
+                    cells={{
+                        data: CountryCell
+                    }}
+                    columnMenu={ColumnMenu}
+                    width="120px"
+                />
+            </Column>
+            <Column title="Performance" groupable={false} sortable={false}>
+                <Column
+                    field="target"
+                    title="Engagement"
+                    cells={{
+                        data: ProgressCell
+                    }}
+                    columnMenu={ColumnMenu}
+                    width="250px"
+                />
+                <Column
+                    field="budget"
+                    title="Budget"
+                    columnMenu={ColumnMenu}
+                    cells={{
+                        data: BudgetCell
+                    }}
+                    width="150px"
+                />
+                <Column
+                    field="rating"
+                    title="Rating"
+                    cells={{
+                        data: RatingCell
+                    }}
+                    columnMenu={ColumnMenu}
+                    width="230px"
+                />
+            </Column>
+            <Column title="Contacts" groupable={false} sortable={false}>
+                <Column field="phone" title="Phone" columnMenu={ColumnMenu} width="230px" />
+                <Column field="address" title="Address" columnMenu={ColumnMenu} width="230px" />
+            </Column>
+        </>
+    );
+
     return (
         <>
             <ExcelExport
@@ -87,76 +157,11 @@ const EmployeesGrid = () => {
                     pageable={{ pageSizes: true }}
                     groupable={true}
                     selectable={true}
-                    rowSpannable={{ valueGetter: (dataItem, field) => `${dataItem.job_title}-${dataItem[field]}` }}
+                    rowSpannable={{ valueGetter: (dataItem: any, field: string) => `${dataItem.job_title}-${dataItem[field]}` }}
                     defaultTake={10}
                     defaultSkip={0}
                 >
-                    <GridToolbar>
-                        <GridSearchBox style={{ width: 210 }} />
-                        <ButtonGroup>
-                            <Button themeColor={'base'} onClick={exportExcel}>
-                                Export to Excel
-                            </Button>
-                            <Button themeColor={'base'} onClick={exportPDF}>
-                                Export to PDF
-                            </Button>
-                        </ButtonGroup>
-                    </GridToolbar>
-                    <Column filterable={false} groupable={false} width={50} columnType="checkbox" />
-                    <Column title="Employee" groupable={false} sortable={false}>
-                        <Column
-                            field="full_name"
-                            title="Contact Name"
-                            columnMenu={ColumnMenu}
-                            cells={{
-                                data: PersonCell
-                            }}
-                            width="250px"
-                        />
-                        <Column field="job_title" title="Job Title" columnMenu={ColumnMenu} width="220px" />
-                        <Column
-                            field="country"
-                            title="Country"
-                            cells={{
-                                data: CountryCell
-                            }}
-                            columnMenu={ColumnMenu}
-                            width="120px"
-                        />
-                    </Column>
-                    <Column title="Performance" groupable={false} sortable={false}>
-                        <Column
-                            field="target"
-                            title="Engagement"
-                            cells={{
-                                data: ProgressCell
-                            }}
-                            columnMenu={ColumnMenu}
-                            width="250px"
-                        />
-                        <Column
-                            field="budget"
-                            title="Budget"
-                            columnMenu={ColumnMenu}
-                            cells={{
-                                data: BudgetCell
-                            }}
-                            width="150px"
-                        />
-                        <Column
-                            field="rating"
-                            title="Rating"
-                            cells={{
-                                data: RatingCell
-                            }}
-                            columnMenu={ColumnMenu}
-                            width="230px"
-                        />
-                    </Column>
-                    <Column title="Contacts" groupable={false} sortable={false}>
-                        <Column field="phone" title="Phone" columnMenu={ColumnMenu} width="230px" />
-                        <Column field="address" title="Address" columnMenu={ColumnMenu} width="230px" />
-                    </Column>
+                    {gridChildren}
                 </KendoGrid>
             </ExcelExport>
             <GridPDFExport
@@ -175,76 +180,11 @@ const EmployeesGrid = () => {
                     pageable={{ pageSizes: true }}
                     groupable={true}
                     selectable={true}
-                    rowSpannable={{ valueGetter: (dataItem, field) => `${dataItem.job_title}-${dataItem[field]}` }}
+                    rowSpannable={{ valueGetter: (dataItem: any, field: string) => `${dataItem.job_title}-${dataItem[field]}` }}
                     defaultTake={10}
                     defaultSkip={0}
                 >
-                    <GridToolbar>
-                        <GridSearchBox style={{ width: 210 }} />
-                        <ButtonGroup>
-                            <Button themeColor={'base'} onClick={exportExcel}>
-                                Export to Excel
-                            </Button>
-                            <Button themeColor={'base'} onClick={exportPDF}>
-                                Export to PDF
-                            </Button>
-                        </ButtonGroup>
-                    </GridToolbar>
-                    <Column filterable={false} groupable={false} width={50} columnType="checkbox" />
-                    <Column title="Employee" groupable={false} sortable={false}>
-                        <Column
-                            field="full_name"
-                            title="Contact Name"
-                            columnMenu={ColumnMenu}
-                            cells={{
-                                data: PersonCell
-                            }}
-                            width="250px"
-                        />
-                        <Column field="job_title" title="Job Title" columnMenu={ColumnMenu} width="220px" />
-                        <Column
-                            field="country"
-                            title="Country"
-                            cells={{
-                                data: CountryCell
-                            }}
-                            columnMenu={ColumnMenu}
-                            width="120px"
-                        />
-                    </Column>
-                    <Column title="Performance" groupable={false} sortable={false}>
-                        <Column
-                            field="target"
-                            title="Engagement"
-                            cells={{
-                                data: ProgressCell
-                            }}
-                            columnMenu={ColumnMenu}
-                            width="250px"
-                        />
-                        <Column
-                            field="budget"
-                            title="Budget"
-                            columnMenu={ColumnMenu}
-                            cells={{
-                                data: BudgetCell
-                            }}
-                            width="150px"
-                        />
-                        <Column
-                            field="rating"
-                            title="Rating"
-                            cells={{
-                                data: RatingCell
-                            }}
-                            columnMenu={ColumnMenu}
-                            width="230px"
-                        />
-                    </Column>
-                    <Column title="Contacts" groupable={false} sortable={false}>
-                        <Column field="phone" title="Phone" columnMenu={ColumnMenu} width="230px" />
-                        <Column field="address" title="Address" columnMenu={ColumnMenu} width="230px" />
-                    </Column>
+                    {gridChildren}
                 </KendoGrid>
             </GridPDFExport>
         </>
